8383f265783b00b6f3456ff0dbb3f6db0e1c9694
Ansharg <anshgarg94161@gmail.com>
2024-06-04 19:33:49 +0530

    folder structuring is given

--- ques 3/src/Pages/AllProductPage.js
+++ ques 3/src/Pages/AllProductPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import axios from 'axios'
+import { BaseUrl } from '../Utils/BaseUrl'
 
 const AllProductPage = () => {
+  axios.get(`${BaseUrl}/AMZ/categories/Laptop/products?top=10&minPrice=1&maxPrice=10000`)
   return (
     <div>
-      
+      all products page
     </div>
   )
 }
---
+++ ques 3/src/Utils/BaseUrl.js
@@ -0,0 +1,1 @@
+export const BaseUrl = "http://20.244.56.144/test/companies"
